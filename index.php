06934e511584e8caa30715abea306eef36013013
Tung Pham <tung.42@gmail.com>
2023-09-15 14:03:24 +0700

    viewport

--- index.php
+++ index.php
@@ -4,7 +4,8 @@ $currentURL = "http" . (isset($_SERVER['HTTPS']) ? "s" : "") . "://" . $_SERVER[
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
     <meta property="og:image" content="utf8_200x200.png">
     <meta property="og:image:width" content="200">
